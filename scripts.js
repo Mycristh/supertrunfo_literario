90b7a9ed3a38afb3a79e929386bd27a494f45deb
Mylena Oliveira <101020021+Mycristh@users.noreply.github.com>
2022-06-28 13:19:25 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,311 @@
+var carta1 = {
+  nome: "Edward Cullen",
+  imagem: "https://cdn.pensador.com/img/authors/ed/wa/edward-cullen-l.jpg",
+  atributos: {
+    poder: 4,
+    beleza: 8,
+    inteligência: 8
+  }
+}
+
+var carta2 = {
+  nome: "Bella Swan",
+  imagem: "http://pm1.narvii.com/7874/275b6e6c5387ed7279360ef32471763a6725f514r1-236-350v2_uhq.jpg",
+  atributos: {
+    poder: 4,
+    beleza: 5,
+    inteligência: 8
+  }
+}
+
+var carta3 = {
+  nome: "Anne Shirley",
+  imagem: "https://imagoi.com/wp-content/uploads/2020/05/anne-with-an-e-anne-shirley-cuthbert.jpg",
+  atributos: {
+    poder: 2,
+    beleza: 4,
+    inteligência: 8
+  }
+}
+
+var carta4 = {
+  nome: "Sherlock Holmes",
+  imagem: "https://i.pinimg.com/474x/18/8d/10/188d10e606030d2d68c66a2421157d07.jpg",
+  atributos: {
+    poder: 3,
+    beleza: 4,
+    inteligência: 10
+  }
+}
+
+var carta5 = {
+  nome: "Jace Herondale",
+  imagem: "https://i.pinimg.com/originals/28/24/5c/28245c6294d1d92e58193cfae7fec970.jpg",
+  atributos: {
+    poder: 7,
+    beleza: 8,
+    inteligência: 7
+  }
+}
+
+var carta6 = {
+  nome: "Tessa Gray",
+  imagem: "https://i.pinimg.com/originals/b6/c0/de/b6c0de582485fdbdb6a61952d36ad566.jpg",
+  atributos: {
+    poder: 8,
+    beleza: 8,
+    inteligência: 8
+  }
+}
+
+var carta7 = {
+  nome: "Lia",
+  imagem: "https://64.media.tumblr.com/0927687ffae1f7d847acf5ef4df7fd04/dca31f75a069b0eb-76/s540x810/31c01ebda90663b780b8d781a864623b422ea233.jpg",
+  atributos: {
+    poder: 4,
+    beleza: 8,
+    inteligência: 8
+  }
+}
+
+var carta8 = {
+  nome: "America Singer",
+  imagem: "http://pm1.narvii.com/6546/38795ff805fcea39107097526f820e716c44dcac_00.jpg",
+  atributos: {
+    poder: 2,
+    beleza: 8,
+    inteligência: 6
+  }
+}
+
+var carta9 = {
+  nome: "Rhysand",
+  imagem: "https://uploads.spiritfanfiction.com/historias/capas/202107/high-school-dxd--a-historia-de-rhysand-balthazar-22589184-010720211717.jpg",
+  atributos: {
+    poder: 10,
+    beleza: 9,
+    inteligência: 6
+  }
+}
+
+var carta10 = {
+  nome: "Hermione Granger",
+  imagem: "https://rockntech.com.br/wp-content/uploads/2013/07/hermione-granger.jpg",
+  atributos: {
+    poder: 8,
+    beleza: 4,
+    inteligência: 10
+  }
+}
+
+var carta11 = {
+  nome: "Elizabeth Bennet",
+  imagem: "https://pbs.twimg.com/profile_images/1377524046/Elizabeth1_400x400.jpg",
+  atributos: {
+    poder: 3,
+    beleza: 4,
+    inteligência: 8
+  }
+}
+
+var carta12 = {
+  nome: "Hercule Poirot",
+  imagem: "https://conteudo.imguol.com.br/c/entretenimento/02/2022/02/11/morte-no-nilo-1644590795585_v2_750x1000.jpg",
+  atributos: {
+    poder: 3,
+    beleza: 2,
+    inteligência: 10
+  }
+}
+
+var carta13 = {
+  nome: "Ryuk",
+  imagem: "http://pm1.narvii.com/6426/748d07c64bb6b9b9b1fc3c783b772e5b4eec0aef_00.jpg",
+  atributos: {
+    poder: 7,
+    beleza: 2,
+    inteligência: 4
+  }
+}
+
+var carta14 = {
+  nome: "L",
+  imagem: "https://i.pinimg.com/550x/ea/f3/88/eaf388a51286206458426d9c9dd204ce.jpg",
+  atributos: {
+    poder: 7,
+    beleza: 3,
+    inteligência: 10
+  }
+}
+
+var carta15 = {
+  nome: "Light Yagami",
+  imagem: "https://1.bp.blogspot.com/-6_Ng4dR2DQ8/YNlZvvaR0NI/AAAAAAAAM5A/LFi3SEJbi8MJ8ySSyxzTc2QNaPcCJ4-MACLcBGAsYHQ/s1600/DEATH.NOTE.full.1909753.jpg",
+  atributos: {
+    poder: 7,
+    beleza: 8,
+    inteligência: 10
+  }
+}
+
+var carta16 = {
+  nome: "Voldemort",
+  imagem: "http://2.bp.blogspot.com/-hvofcScgU-A/ToX0feiRJmI/AAAAAAAAAUA/igGdff80XtI/w1200-h630-p-k-no-nu/Lord%2BVoldemort.jpg",
+  atributos: {
+    poder: 9,
+    beleza: 1,
+    inteligência: 8
+  }
+}
+
+var carta17 = {
+  nome: "Rony Weasley",
+  imagem: "https://pm1.narvii.com/6434/d5c39b760fa02863487e50e38e4e4352e56a0db9_hq.jpg",
+  atributos: {
+    poder: 8,
+    beleza: 4,
+    inteligência: 4
+  }
+}
+
+var carta18 = {
+  nome: "Harry Potter",
+  imagem: "https://wikiimg.tojsiabtv.com/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg",
+  atributos: {
+    poder: 8,
+    beleza: 7,
+    inteligência: 8
+  }
+}
+
+var carta19 = {
+  nome: "Katniss Everdeen",
+  imagem: "http://2.bp.blogspot.com/-wkrALUuGKBE/T3CesHSeEcI/AAAAAAABQa0/ki59jsgIOis/s1600/lawrence+hunger.jpg",
+  atributos: {
+    poder: 7,
+    beleza: 7,
+    inteligência: 8
+  }
+}
+
+var carta20 = {
+  nome: "R",
+  imagem: "https://br.web.img3.acsta.net/r_1280_720/medias/nmedia/18/88/60/64/19995366.jpg",
+  atributos: {
+    poder: 4,
+    beleza: 4,
+    inteligência: 6
+  }
+}
+
+var carta21 = {
+  nome: "Aslan",
+  imagem: "http://pm1.narvii.com/6365/1d3988fff896f1240d0cfd820ac81eebdd670fd0_00.jpg",
+  atributos: {
+    poder: 10,
+    beleza: 10,
+    inteligência: 10
+  }
+}
+
+
+
+var cartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14, carta15, carta16, carta17, carta18, carta19, carta20, carta21]
+var cartaMaquina
+var cartaJogador
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * 21)
+  cartaMaquina = cartas[numeroCartaMaquina]
+  
+  var numeroCartaJogador = parseInt(Math.random() * 21)
+  while (numeroCartaMaquina == numeroCartaJogador) {
+   numeroCartaJogador = parseInt(Math.random() * 21) 
+  }
+  cartaJogador = cartas[numeroCartaJogador]
+  console.log(cartaJogador)
+   document.getElementById("btnSortear").disabled = true
+  document.getElementById("btnJogar").disabled = false
+  exibirCartaJogador() 
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributo = document.getElementsByName("atributo");
+  for (var i = 0; i < radioAtributo.length; i++) {
+    if (radioAtributo[i].checked) {
+      return radioAtributo[i].value;
+    }
+  }
+}
+
+function jogar() {
+  console.log("chamou");
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var divResultado = document.getElementById("resultado");
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>Você venceu!</p>";
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>Você perdeu</p>";
+  } else {
+    htmlResultado = "<p class='resultado-final'>Empatou</p>";
+  }
+  divResultado.innerHTML = htmlResultado;
+
+  document.getElementById("btnJogar").disabled = true;
+  exibirCartaMaquina();
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
